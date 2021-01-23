262977448a54be717f915fb3251ba4b19cfac25c
shanberochka <58555759+shanberochka@users.noreply.github.com>
2021-01-23 17:59:19 +0500

    Add files via upload

---
+++ picture-viewer/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.showButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.backgroundButton = New System.Windows.Forms.Button()
+        Me.closeButton = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.CheckBox1, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.FlowLayoutPanel1, 1, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(712, 455)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.TableLayoutPanel1.SetColumnSpan(Me.PictureBox1, 2)
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(4, 4)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(704, 401)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CheckBox1.Location = New System.Drawing.Point(4, 413)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(98, 38)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Stretch"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.showButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.clearButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.backgroundButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.closeButton)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(110, 413)
+        Me.FlowLayoutPanel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(598, 38)
+        Me.FlowLayoutPanel1.TabIndex = 2
+        '
+        'showButton
+        '
+        Me.showButton.AutoSize = True
+        Me.showButton.Location = New System.Drawing.Point(4, 0)
+        Me.showButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.showButton.Name = "showButton"
+        Me.showButton.Size = New System.Drawing.Size(147, 34)
+        Me.showButton.TabIndex = 0
+        Me.showButton.Text = "Показать рисунок"
+        Me.showButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.AutoSize = True
+        Me.clearButton.Location = New System.Drawing.Point(159, 0)
+        Me.clearButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(144, 34)
+        Me.clearButton.TabIndex = 1
+        Me.clearButton.Text = "Очистить рисунок"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'backgroundButton
+        '
+        Me.backgroundButton.AutoSize = True
+        Me.backgroundButton.Location = New System.Drawing.Point(311, 0)
+        Me.backgroundButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.backgroundButton.Name = "backgroundButton"
+        Me.backgroundButton.Size = New System.Drawing.Size(176, 34)
+        Me.backgroundButton.TabIndex = 2
+        Me.backgroundButton.Text = "Установить цвет фона"
+        Me.backgroundButton.UseVisualStyleBackColor = True
+        '
+        'closeButton
+        '
+        Me.closeButton.AutoSize = True
+        Me.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.closeButton.Location = New System.Drawing.Point(495, 0)
+        Me.closeButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.closeButton.Name = "closeButton"
+        Me.closeButton.Size = New System.Drawing.Size(100, 34)
+        Me.closeButton.TabIndex = 3
+        Me.closeButton.Text = "Закрыть"
+        Me.closeButton.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" &
+    "s (*.*)|*.*"
+        Me.OpenFileDialog1.Title = " Выбор файла изображения"
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.showButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.closeButton
+        Me.ClientSize = New System.Drawing.Size(712, 455)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Программа просмотра изображений"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents showButton As Button
+    Friend WithEvents clearButton As Button
+    Friend WithEvents backgroundButton As Button
+    Friend WithEvents closeButton As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ColorDialog1 As ColorDialog
+End Class
---
+++ picture-viewer/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub ShowButton_Click(sender As Object, e As EventArgs) Handles showButton.Click
+        ' Show the Open File dialog. If the user clicks OK, load the
+        ' picture that the user chose.
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            PictureBox1.Load(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        ' Clear the picture.
+        PictureBox1.Image = Nothing
+    End Sub
+
+    Private Sub BackgroundButton_Click(sender As Object, e As EventArgs) Handles backgroundButton.Click
+        ' Show the color dialog box. If the user clicks OK, change the
+        ' PictureBox control's background to the color the user chose.
+        If ColorDialog1.ShowDialog() = DialogResult.OK Then
+            PictureBox1.BackColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        ' If the user select the Stretch check box,
+        ' change the PictureBox's
+        ' SizeMode property to "Stretch". If the user clears
+        ' the check box, change it to "Normal".
+        If CheckBox1.Checked Then
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        Else
+            PictureBox1.SizeMode = PictureBoxSizeMode.Normal
+
+        End If
+    End Sub
+End Class
